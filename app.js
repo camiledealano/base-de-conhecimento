34ebcc4fa597d5717469a4f886174ebdb064c366
camiledealano <alanocamile@gmail.com>
2023-09-03 17:07:13 -0300

    estrutura de pastas e bootstrap via cdn (pfv alguem faz o front)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 // RODAR COM npx nodemon app.js para rodar nodemon
+app.use("/home", express.static("./public/index.html"));
+app.get("/", (req,res) => {
+    res.redirect("/home");
+})
 
 app.listen(9000, () => {
     console.log("Aplicação rodando em http://localhost:9000 !");
-})
-
-app.get("/", (req,res) => {
-    res.send("app rodando")
 })
