7697bf9b10b4090949c90c1032947f826b6c17ac
crissipires <crissiano_pires@hotmail.com>
2023-09-27 14:55:50 -0300

    ajuste

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,4 +1,12 @@
 function isAdmin(req, res, next) {
+    if (req.session.user.author_status === null) {
+        req.session.message = {
+            type:'danger',
+            message:'Você precisa estar logado como administrador!'
+        };
+        return res.redirect('/home');
+    }
+
     if(req.session.user != null ){  
         if(!req.session.user.author_level === 'adminstrador'){
             req.session.message = {
@@ -6,15 +14,7 @@ function isAdmin(req, res, next) {
                 message:'Você precisa ser um administrador!'
             };
             return res.redirect('/home');
-        }
-
-        if (!req.session.user.author_status === 'Ativo') {
-            req.session.message = {
-                type:'danger',
-                message:'Você precisa estar ativo!'
-            };
-            return res.redirect('/home');
-        }
+        }      
     } 
     return next();
 }
